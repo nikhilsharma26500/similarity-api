8c2edfd5593a1c8354c98add39a428744d62ff90
Nikhil <nikhilsharma26500@outlook.com>
2023-07-11 03:14:41 +0530

    configured authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import { NextAuthOptions } from "next-auth";
+import { db } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+
+function getGoogleCredentials() {
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+    if (!clientId || clientId.length === 0) {
+        throw new Error("No client ID found for Google provider set")
+    }
+
+    if(!clientSecret || clientSecret.length === 0) {
+        throw new Error("No client secret found for Google provider set")
+    }
+
+    return {clientId, clientSecret}
+}
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/login",
+    },
+    providers: [
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        }),
+    ]
+}
