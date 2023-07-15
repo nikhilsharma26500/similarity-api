6af43d115fb02bc48fc751326becbfa33798b064
Nikhil <nikhilsharma26500@outlook.com>
2023-07-16 00:27:32 +0530

    defined auth and global declarations for it

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,32 +4,44 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 
 function getGoogleCredentials() {
-    const clientId = process.env.GOOGLE_CLIENT_ID
-    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-    if (!clientId || clientId.length === 0) {
-        throw new Error("No client ID found for Google provider set")
-    }
+  if (!clientId || clientId.length === 0) {
+    throw new Error("No client ID found for Google provider set");
+  }
 
-    if(!clientSecret || clientSecret.length === 0) {
-        throw new Error("No client secret found for Google provider set")
-    }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("No client secret found for Google provider set");
+  }
 
-    return {clientId, clientSecret}
+  return { clientId, clientSecret };
 }
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(db),
-    session: {
-        strategy: "jwt",
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+      }
+
+      return session;
     },
-    pages: {
-        signIn: "/login",
-    },
-    providers: [
-        GoogleProvider({
-            clientId: getGoogleCredentials().clientId,
-            clientSecret: getGoogleCredentials().clientSecret,
-        }),
-    ]
-}
+  },
+};
